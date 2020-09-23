2eb3ea36cd837618f16407886665c7f19c534729
Mihail <colty.gaming@gmail.com>
2020-09-23 08:24:24 +0200

    unity assets

---
+++ Assets/CivillianCollider.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CivillianCollider : MonoBehaviour
+{
+    public GameObject go1, go2, go3;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.name == "Plane")
+        {
+            go1.SetActive(true);
+        }
+        else
+        {
+            go2.SetActive(true);
+            go1.SetActive(false);
+            go3.SetActive(false);
+        }
+         
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Plane")
+        {
+            go1.SetActive(false);
+        }
+        else
+        {
+            go2.SetActive(false);
+            go1.SetActive(true);
+            go3.SetActive(true);
+        }
+    }
+}
---
+++ Assets/OtherCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherCollision : MonoBehaviour
+{
+    public GameObject go1;
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.name == "Plane")
+        {
+            go1.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Plane")
+        {
+            go1.SetActive(false);
+        }
+    }
+}
